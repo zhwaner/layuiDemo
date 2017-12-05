33b79b6e3126ae812773a474bb301bec2ad93fcc
zhwaner <zhangwan@keywa.com>
2017-12-05 14:00:31 +0800

    tpl.js构建

--- tasks/tpl.js
+++ tasks/tpl.js
@@ -1,24 +1,29 @@
 const gulp = require('gulp')
+const tmod = require('gulp-tmod') // TmodJS和artTemplate语法一脉相承
 
 let task = {
-  run (handlePath) {
-  	console.log(handlePath)
-    // glob.sync(handlePath).forEach(path => {
-    //   let pageName = path.split('/').pop().split('.')[0]
-    //   gulp.src(path)
-    //     .on('end', () => {
-    //       console.log(`html task ${pageName} done!`)
-    //     })
-    //     .pipe(fileInclude({
-    //       prefix: '@@',
-    //       basepath: './src/common/include/'
-    //     }))
-    //     .pipe(gulpReplace('<!-- page css -->', `<link rel="stylesheet" href="css/${pageName}.css">`))
-    //     .pipe(gulpReplace('<!-- page js -->', `<script src="js/${pageName}.js"></script>`))
-    //     .pipe(htmlbeautify(beautifyOptions))
-    //     .pipe(htmlmin(minOptions))
-    //     .pipe(gulp.dest('dist'))
-    // })
+  run (path, cb) {
+    var pathArr = path.split('\\')
+    var tplName = pathArr[pathArr.length - 3] // 获取所在目录名，such as index
+    pathArr.pop()
+    var tplPath = pathArr.join('/')
+    var distUrl = './src/app/' + tplName + '/js' // tpl打包到js目录tpl.js
+    if (/common/.test(pathArr)) {
+      distUrl = './src/common/js'
+    }
+
+    gulp.src([tplPath + '/*.tpl'])
+    .on('end', () => {
+      cb && cb()
+    })
+    .pipe(tmod({
+      'templateBase': tplPath,
+      'runtime': 'tpl.js', // 输出时的运行名称
+      'combo': true, // 是否合并模板
+      'cache': false, // 是否开启编译缓存
+      'syntax': 'simple' // 定义模板采用哪种语法，可选：simple、native
+    }))
+    .pipe(gulp.dest(distUrl))
   }
 }
 
